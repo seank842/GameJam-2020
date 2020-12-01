ba179a25fd7ffd2deb74c68018325c8de7e6cf12
Sean Knox <seank842@live.co.uk>
2020-12-01 19:09:20 +0000

    Same as previous commit, Git failed on last commit

---
+++ Swarm Platformer/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject CreditsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuButton();
+    }
+
+    public void StartButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level_One");
+    }
+
+    public void CreditsButton()
+    {
+        MainMenu.SetActive(false);
+        CreditsMenu.SetActive(true);
+    }
+
+    public void MainMenuButton()
+    {
+        MainMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
