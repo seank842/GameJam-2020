febc7f6d28faa087185f53cd24ddf3644e700073
Sean Knox <seank842@live.co.uk>
2020-12-02 22:37:06 +0000

    Added Score, Survivor, and Timer display; Score and Survior is fake data; Timer currently will just exit the game, a game over screen needws to be added

--- Swarm Platformer/Assets/Scripts/GlobalSceneManager.cs
+++ Swarm Platformer/Assets/Scripts/GlobalSceneManager.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GlobalSceneManager : MonoBehaviour
 {
+    #region Fields
+    [SerializeField]
+    private Text _survivorsText;
+    [SerializeField]
+    private Text _timerText;
+    private TimeSpan _timeLeft;
+    #endregion
+
+    #region Properties 
+    public Text SurvivorsText
+    {
+        get => _survivorsText;
+        set => _survivorsText = value;
+    }
+
+    public Text TimerText
+    {
+        get => _timerText;
+        set => _timerText = value;
+    }
+
+    public List<GameObject> Players;
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Players = new List<GameObject>();
+        _timeLeft = new TimeSpan(0, 0, 30);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _timeLeft -= TimeSpan.FromSeconds(Time.deltaTime);
+        if(_timeLeft < TimeSpan.FromSeconds(0))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+        _survivorsText.text = $"Survivors: {Players.Count}";
+        _timerText.text = _timeLeft.ToString(@"m\:ss\.fff");
     }
 }
--- Swarm Platformer/Assets/Scripts/MainMenuManager.cs
+++ Swarm Platformer/Assets/Scripts/MainMenuManager.cs
@@ -42,6 +42,10 @@ public class MainMenuManager : MonoBehaviour
 
     public void QuitButton()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
