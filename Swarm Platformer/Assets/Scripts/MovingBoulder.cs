972c1f469d38c1a8dc0ae8e082392a6509cf7248
Scott <scott.holdsworth@hotmail.com>
2020-12-04 23:37:49 +0000

    Simple Moving Platform
    The platform moves via a simple animation.

---
+++ Swarm Platformer/Assets/Scripts/MovingBoulder.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBoulder : MonoBehaviour
+{
+    private Vector3 posA;
+    private Vector3 posB;
+    private Vector3 nextPos;
+
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Transform platform;
+
+    [SerializeField]
+    private Transform transformB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posB = platform.localPosition;
+        posB = transform.localPosition;
+        nextPos = posB;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        platform.localPosition = Vector3.MoveTowards(platform.localPosition, nextPos, speed * Time.deltaTime);
+        if (Vector3.Distance(platform.localPosition,nextPos) <= 0.1)
+        {
+            ChangeDestination();
+        }
+    }
+    private void ChangeDestination()
+    {
+        nextPos = nextPos != posA ? posA : posB;
+    }
+}
