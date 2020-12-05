bd0a0285453448f044f100da5294076eaa326975
Scott <scott.holdsworth@hotmail.com>
2020-12-05 15:41:51 +0000

    Simple level
    Created a simple level, with obstacles (they don't kill the player yet).

---
+++ Swarm Platformer/Assets/Scripts/FallingFloor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingFloor : MonoBehaviour
+{
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+           
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+           
+        }
+    }
+}
