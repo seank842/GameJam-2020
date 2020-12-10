9ebebd62afb38b0a88987736a0ebd93a23b9fb10
Sam <samuelmcdouall@gmail.com>
2020-12-10 21:07:13 +0000

    Made saws moving

--- Swarm Platformer/Assets/Scripts/SawBlade.cs
+++ Swarm Platformer/Assets/Scripts/SawBlade.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class SawBlade : MonoBehaviour
 {
-    
+    float timer;
     // Update is called once per frame
+    private void Start()
+    {
+        timer = 0.0f;
+    }
     void Update()
     {
-        transform.Rotate(new Vector3(0f, 0f, 1f));
+        if (!GlobalSceneManager.GameOver) // Or Pause or Victory
+        {
+            transform.Rotate(new Vector3(0f, 0f, 1f));
+            timer += Time.deltaTime;
+            float new_y_position = transform.position.y + 0.15f * -Mathf.Cos(1.0f * timer);
+            transform.position = new Vector3(transform.position.x, new_y_position, transform.position.z);
+        }
     }
 }
